c0d0d7b43b539bb6f86e885fe23c70b94b3018a6
Валентин Немцев <v-nemcev@yandex-team.ru>
2019-09-08 15:30:32 +0300

    Tests for caret pos reducer

--- src/reducer.ts
+++ src/reducer.ts
@@ -4,28 +4,62 @@ import {clamp} from 'lodash';
 import * as actions from './actions';
 import {CaretPos} from './buildTokens';
 
+type Lines = string[];
 interface EditorState {
-    lines: string[];
+    lines: Lines;
     caretPos: CaretPos;
 }
 
-const reducer = createReducer<EditorState, ActionType<typeof actions>>()
-    .handleAction(actions.setCaretAction, (state, {payload: caretPos}) => ({
-        ...state,
-        caretPos,
+function clampCaretPos(
+    caretPos: CaretPos,
+    lines: Lines,
+    {
+        linePastEnd = false,
+        colPastEnd = false,
+    }: {linePastEnd?: boolean; colPastEnd?: boolean} = {},
+): CaretPos {
+    const line = clamp(
+        caretPos.line,
+        0,
+        linePastEnd ? Number.MAX_SAFE_INTEGER : lines.length,
+    );
+    const col = clamp(
+        caretPos.col,
+        0,
+        colPastEnd ? Number.MAX_SAFE_INTEGER : (lines[line] || []).length,
+    );
+    return {line, col};
+}
+
+const setCaretPos = clampCaretPos;
+
+function moveCaretPos(
+    caretPos: CaretPos,
+    delta: CaretPos,
+    lines: Lines,
+): CaretPos {
+    const colPastEnd = delta.col === 0;
+    const {line, col} = clampCaretPos(caretPos, lines, {colPastEnd});
+    return clampCaretPos(
+        {line: line + delta.line, col: col + delta.col},
+        lines,
+        {colPastEnd},
+    );
+}
+
+const reducer = createReducer<EditorState, ActionType<typeof actions>>({
+    lines: [],
+    caretPos: {line: 0, col: 0},
+})
+    .handleAction(actions.setCaretAction, ({lines}, {payload: caretPos}) => ({
+        lines,
+        caretPos: setCaretPos(caretPos, lines),
     }))
     .handleAction(
         actions.moveCaretAction,
         ({lines, caretPos}, {payload: delta}) => ({
             lines,
-            caretPos: {
-                line: clamp(caretPos.line + delta.line, 0, lines.length - 1),
-                col: clamp(
-                    caretPos.col + delta.col,
-                    0,
-                    Math.max(caretPos.col, (lines[caretPos.line] || '').length),
-                ),
-            },
+            caretPos: moveCaretPos(caretPos, delta, lines),
         }),
     );
 
